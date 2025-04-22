fbf3ef1d490d97c5fe9d092b9ce5aa7061ea645a
TheOneYouDeserve <ItDoesntExist@gmail.com>
2025-04-23 00:00:16 +0530

    added core packages name

--- src/metadb/core.py
+++ src/metadb/core.py
@@ -1,5 +1,4 @@
-from .descion_tree import descion_treemetadb
-from .frequent_pattern_mining import frequent_pattern_miningmetadb
-from .hierarchial_clustering import hierarchial_clusteringmetadb
-from .Kmeans import kmeansmetadb
-from .naive_bayes import naive_bayesmetadb
+def corePackages():
+    print("""
+          pip install pandas==1.5.3 numpy==1.24.3 matplotlib==3.7.1 scikit-learn==1.2.2 scipy==1.10.1 seaborn==0.12.2 mlxtend==0.22.0
+          """)
