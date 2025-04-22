35c287d9dfcaedd5039ba23bfd941b20d479b0b6
TheOneYouDeserve <ItDoesntExist@gmail.com>
2025-04-22 23:23:31 +0530

    updated init to include new functions of algorithm

--- src/metadb/__init__.py
+++ src/metadb/__init__.py
@@ -1,6 +1,6 @@
-from .descion_tree import descion_treemetadb
-from .frequent_pattern_mining import frequent_pattern_miningmetadb
-from .hierarchial_clustering import hierarchial_clusteringmetadb
-from .Kmeans import Kmeansmetadb
-from .naive_bayes import naive_bayesmetadb
+from .decisionTree import (decision_treemetadb,decision_treemetab2)
+from .frequent_pattern_mining import (frequent_pattern_miningmetadb)
+from .hierarchial_clustering import (hierarchial_clusteringmetadb,hierarchial_clusteringmetadb2)
+from .Kmeans import (Kmeansmetadb,Kmeansmetadb2)
+from .naive_bayes import (naive_bayesmetadb,naive_bayesmetadb2)
 
