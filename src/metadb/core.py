d5108181b76e7c6957ca5972c46fa91a58aa7c27
TheOneYouDeserve <ItDoesntExist@gmail.com>
2025-04-23 12:15:24 +0530

    added dataset links

--- src/metadb/core.py
+++ src/metadb/core.py
@@ -1,4 +1,6 @@
 def corePackages():
     print("""
           pip install pandas==1.5.3 numpy==1.24.3 matplotlib==3.7.1 scikit-learn==1.2.2 scipy==1.10.1 seaborn==0.12.2 mlxtend==0.22.0
+          
+          https://tinyurl.com/dmbiexam
           """)
